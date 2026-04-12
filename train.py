dc2f63e305e6ca77f34864667b3b4e49685764c0
Sudharsan Murugesan <sudharsan.muru@gmail.com>
2026-04-13 00:56:10 +0530

    tried some complex reward system but not working current and going back to simpler system at the moment

--- train.py
+++ train.py
@@ -2,13 +2,14 @@ import math
 import torch
 import torch.nn as nn
 
-def bellmans_update(active_network,dormant_network,buffer_exp,gamma):
 
-    states_batch = torch.tensor([t[0] for t in buffer_exp], dtype=torch.float32)
-    actions_batch = torch.tensor([t[1] for t in buffer_exp], dtype=torch.int64)
-    next_states_batch = torch.tensor([t[2] for t in buffer_exp], dtype=torch.float32)
-    rewards_batch = torch.tensor([t[3] for t in buffer_exp], dtype=torch.float32)
-    dones_batch = torch.tensor([t[4] for t in buffer_exp], dtype=torch.float32)
+def bellmans_update(active_network,dormant_network,buffer_exp,gamma,device):
+
+    states_batch = torch.tensor([t[0] for t in buffer_exp], dtype=torch.float32).to(device)
+    actions_batch = torch.tensor([t[1] for t in buffer_exp], dtype=torch.int64).to(device)
+    next_states_batch = torch.tensor([t[2] for t in buffer_exp], dtype=torch.float32).to(device)
+    rewards_batch = torch.tensor([t[3] for t in buffer_exp], dtype=torch.float32).to(device)
+    dones_batch = torch.tensor([t[4] for t in buffer_exp], dtype=torch.float32).to(device)
 
     # target — computed from target network, no gradients needed
     with torch.no_grad():
@@ -23,16 +24,30 @@ def bellmans_update(active_network,dormant_network,buffer_exp,gamma):
     loss = nn.MSELoss()(predicted, target)
     return loss
 
+
 def compute_reward(state_tensor):
     theta_normalized = state_tensor[2].item() % (2 * math.pi)
-    upright = abs(theta_normalized - math.pi) < 0.5  # within ~28° of upright
-    fallen = abs(theta_normalized - math.pi) > 0.5
+    upright = abs(theta_normalized - math.pi) < 0.8
+
+    out_of_bounds = state_tensor[0].item() < 150 or state_tensor[0].item() > 650
+    fallen = not upright or out_of_bounds
+
+    angle_reward = 1 - abs(theta_normalized - math.pi) / math.pi
+    position_reward = 1 - abs(state_tensor[0].item() - 400) / 400
+    wall_distance = abs(state_tensor[0].item() - 400) / 400
+    wall_penalty = wall_distance ** 2
+
     if upright:
-        reward = 1
-    elif state_tensor[0].item() <= 50 or state_tensor[0].item() >= 750:
-        reward = -5
+        stillness_bonus = 1 / (1 + abs(state_tensor[1].item()) * 0.1)
+        velocity_penalty = (abs(state_tensor[1].item()) / 60) ** 2 * 2
+        rew = 2*angle_reward + position_reward + stillness_bonus - velocity_penalty - 4*wall_penalty
     else:
-        reward = -1
-    return reward, fallen
+        velocity_penalty = (abs(state_tensor[1].item()) / 60) ** 2
+        rew = 2*angle_reward + position_reward - velocity_penalty - 4*wall_penalty
+
+    if out_of_bounds:
+        rew = -20
+
+    return rew, fallen
 
 
