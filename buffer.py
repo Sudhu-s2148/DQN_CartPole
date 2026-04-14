157d9e7854a482379638a1ea4f0ddb9b5dbacd64
Sudharsan Murugesan <sudharsan.muru@gmail.com>
2026-04-14 22:33:21 +0530

    updated the code to use the gymnasium cartpole. working on the model currently

--- buffer.py
+++ buffer.py
@@ -2,7 +2,7 @@ from collections import deque
 import random
 class Buffer:
     def __init__(self):
-        self.deq = deque(maxlen = 10000)
+        self.deq = deque(maxlen = 100000)
     def __len__(self):
         return len(self.deq)
     def push(self, state, action, next_state, reward, done):
--- train.py
+++ train.py
@@ -1,23 +1,35 @@
-import math
+import numpy as np
 import agent as A
 import buffer
-import statics5
 import torch
 import torch.nn as nn
-online_network = A.agent()
-offline_network = A.agent()
+import gymnasium
+import os
+
+training = True
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+env = gymnasium.make("CartPole-v1")
+online_network = A.agent().to(device)
+offline_network = A.agent().to(device)
 experiences = buffer.Buffer()
 gamma = 0.99
+epsilon = 1
+epsilon_min = .05
+decay = .9995
+episode = 5000
+step = 500
 
-#reward system
+optimizer = torch.optim.Adam(online_network.parameters(), lr=0.001)
 
-def bellmans_update(active_network,dormant_network,buffer_exp,gamma):
+def bellmans_update(active_network,dormant_network,buffer_exp,gamma, device):
 
-    states_batch = torch.tensor([t[0] for t in buffer_exp], dtype=torch.float32)
-    actions_batch = torch.tensor([t[1] for t in buffer_exp], dtype=torch.int64)
-    next_states_batch = torch.tensor([t[2] for t in buffer_exp], dtype=torch.float32)
-    rewards_batch = torch.tensor([t[3] for t in buffer_exp], dtype=torch.float32)
-    dones_batch = torch.tensor([t[4] for t in buffer_exp], dtype=torch.float32)
+    states_batch = torch.tensor(np.array([t[0] for t in buffer_exp]), dtype=torch.float32).to(device)
+    actions_batch = torch.tensor([t[1] for t in buffer_exp], dtype=torch.int64).to(device)
+    next_states_batch = torch.tensor(np.array([t[2] for t in buffer_exp]), dtype=torch.float32).to(device)
+    rewards_batch = torch.tensor([t[3] for t in buffer_exp], dtype=torch.float32).to(device)
+    dones_batch = torch.tensor([t[4] for t in buffer_exp], dtype=torch.float32).to(device)
 
     # target — computed from target network, no gradients needed
     with torch.no_grad():
@@ -32,23 +44,69 @@ def bellmans_update(active_network,dormant_network,buffer_exp,gamma):
     loss = nn.MSELoss()(predicted, target)
     return loss
 
-optimizer = torch.optim.Adam(online_network.parameters(), lr=0.001)
-epsilon = 1
-
-state = statics5.state_before_action
-state_tensor = torch.tensor(state, dtype=torch.float32)
-
+total_overall_steps = 0
+total_steps = 0
+best_avg_reward = 0
 #episode loop
-for j in range(10):
-    #step loop
+if training:
+    print("started")
+    for j in range(episode):
+        save_path = "best_agent.pth"
+        step_count = 0
+        state,_ = env.reset()
+        state_tensor = torch.tensor(state, dtype=torch.float32).to(device)
+        for i in range(step):
+            total_overall_steps+=1
+            step_count+=1
+            action = online_network.choice(state_tensor,epsilon)# forward pass
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            experiences.push(state,action,next_state,reward,terminated)
+
+            state = next_state
+            state_tensor = torch.tensor(state, dtype=torch.float32).to(device)
+            if len(experiences)>1000:
+                batch = experiences.sample(64)
+                optimizer.zero_grad()  # reset gradients
+                loss = bellmans_update(online_network,offline_network,batch,gamma,device) # compute loss
+                loss.backward() # compute gradients
+                optimizer.step()# update weights
+
+            if total_overall_steps % 1000 == 0:
+                offline_network.load_state_dict(online_network.state_dict())
+            if terminated or truncated:
+                break
+        total_steps += (i + 1)
+
+        if (j + 1) % 100 == 0:
+            current_avg = total_steps / 100
+
+            if current_avg >= best_avg_reward:
+                best_avg_reward = current_avg
+                torch.save(online_network.state_dict(), save_path)
+                print(f"--> New Best Model Saved! Avg Steps: {best_avg_reward:.1f}")
+            print(f"Episode: {j+1} | Avg Steps (last 100): {total_steps/100:.1f} | Epsilon: {epsilon:.3f}")
+            total_steps = 0
+
+        epsilon = max(epsilon * decay, epsilon_min)
+    env.close()
+else:
+    model = A.agent()
+    model.to(device)
+    if os.path.exists("best_cartpole_model.pth"):
+        model.load_state_dict(torch.load("best_cartpole_model.pth", map_location=device))
+        model.eval()
+        print("Model loaded successfully!")
+    else:
+        print("File not found, using fresh model.")
+    env = gymnasium.make("CartPole-v1", render_mode="human")
+    state, _ = env.reset()
     done = False
-    for i in range(500):
-
-        action = online_network.choice(state_tensor,epsilon)  # forward pass
-        next_state = statics5.state_after_action
-        next_state_tensor = torch.tensor(next_state, dtype=torch.float32)
-
-        theta_normalized = state_tensor[2].item() % (2 * math.pi)
+    while not done:
+        action = model.choice(torch.tensor(state, dtype=torch.float32).to(device), epsilon=0)  # epsilon=0 means no randomness
+        state, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+    env.close()
+"""theta_normalized = state_tensor[2].item() % (2 * math.pi)
         fallen = theta_normalized > 30 * math.pi / 180 and theta_normalized < (2 * math.pi - 30 * math.pi / 180)
         upright = theta_normalized < 20 * math.pi / 180 or theta_normalized > (2 * math.pi - 20 * math.pi / 180)
         if upright:
@@ -60,17 +118,4 @@ for j in range(10):
         else:
             reward = -1
 
-        if i == 499 or fallen: done = True
-
-        experiences.push(state,action,next_state,reward,done)
-        optimizer.zero_grad()            # reset gradients
-        if i>64:
-            batch = experiences.sample(64)
-            loss = bellmans_update(online_network,offline_network,batch,gamma) # compute loss
-            loss.backward() # compute gradients
-            optimizer.step()# update weights
-        state = next_state
-        state_tensor = next_state_tensor
-        if done: break
-    offline_network.load_state_dict(online_network.state_dict())
-    epsilon *= .99
+        if i == 499 or fallen: done = True"""
